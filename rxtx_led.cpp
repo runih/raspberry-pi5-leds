fa8d9583cc6ab63715e711a971589b8a9db11fd4
Rúni H.Hansen <runi.hansen@okkara.net>
2025-12-09 18:48:03 +0100

    feat: name threads for RX and TX packet monitoring
    Add thread naming using pthread_setname_np for RX and TX monitor threads.
    Thread names are set to "rx_monitor" and "tx_monitor" for easier debugging
    and process inspection. This improves traceability in multi-threaded
    environments and aids system monitoring tools.

--- rxtx_led.cpp
+++ rxtx_led.cpp
@@ -18,69 +18,70 @@ const string PWR_DELAY_OFF = "/sys/class/leds/PWR/delay_off";
 atomic<bool> running{true};
 
 void write_sysfs(const string& path, const string& value) {
-    ofstream ofs(path);
-    if (ofs) ofs << value;
+  ofstream ofs(path);
+  if (ofs) ofs << value;
 }
 
 string read_sysfs(const string& path) {
-    ifstream ifs(path);
-    string val;
-    if (ifs) ifs >> val;
-    return val;
+  ifstream ifs(path);
+  string val;
+  if (ifs) ifs >> val;
+  return val;
 }
 
-void monitor_packets(const string& eth_if, const string& type, const string& led_shot, const string& msg) {
-    string stat_file = "/sys/class/net/" + eth_if + "/statistics/" + type + "_packets";
-    string prev_str = read_sysfs(stat_file);
-    unsigned long prev = prev_str.empty() ? 0 : stoul(prev_str);
+void monitor_packets(const string& eth_if, const string& type, const string& led_shot, const string& msg, const string& thread_name) {
+  pthread_setname_np(pthread_self(), thread_name.substr(0, 15).c_str()); // max 16 chars including null
+  string stat_file = "/sys/class/net/" + eth_if + "/statistics/" + type + "_packets";
+  string prev_str = read_sysfs(stat_file);
+  unsigned long prev = prev_str.empty() ? 0 : stoul(prev_str);
 
-    while (running) {
-        string cur_str = read_sysfs(stat_file);
-        if (cur_str.empty()) break;
-        unsigned long cur = stoul(cur_str);
+  while (running) {
+    string cur_str = read_sysfs(stat_file);
+    if (cur_str.empty()) break;
+    unsigned long cur = stoul(cur_str);
 
-        if (cur > prev) {
-            cout << msg << ": " << (cur - prev) << " packets" << endl;
-            if (type == "tx") {
-              write_sysfs(led_shot, "1");
-              this_thread::sleep_for(chrono::milliseconds(50));
-              write_sysfs(led_shot, "0");
-            } else {
-              write_sysfs(led_shot, "0");
-              this_thread::sleep_for(chrono::milliseconds(50));
-              write_sysfs(led_shot, "1");
-            }
-            prev = cur;
-        }
+    if (cur > prev) {
+      cout << msg << ": " << (cur - prev) << " packets" << endl;
+      if (type == "tx") {
+        write_sysfs(led_shot, "1");
         this_thread::sleep_for(chrono::milliseconds(50));
+        write_sysfs(led_shot, "0");
+      } else {
+        write_sysfs(led_shot, "0");
+        this_thread::sleep_for(chrono::milliseconds(50));
+        write_sysfs(led_shot, "1");
+      }
+      prev = cur;
     }
+    this_thread::sleep_for(chrono::milliseconds(50));
+  }
 }
 
 int main(int argc, char* argv[]) {
-    string eth_if = (argc > 1) ? argv[1] : "end0";
-    string state = "";
+  string eth_if = (argc > 1) ? argv[1] : "end0";
+  string state = "";
 
-    while (true) {
-        if (fs::is_directory("/sys/class/net/" + eth_if)) {
-            state = "active";
-            running = true;
-            thread tx_thread(monitor_packets, eth_if, "tx", TX_LED_SHOT, "Data transmitted");
-            thread rx_thread(monitor_packets, eth_if, "rx", RX_LED_SHOT, "Data received");
-            tx_thread.join();
-            rx_thread.join();
-            running = false;
-        } else {
-            if (state != "waiting") {
-                cerr << "Network interface " << eth_if << " does not exist. Please check the interface name." << endl;
-                write_sysfs(RX_LED_SHOT, "1");
-                write_sysfs(TX_LED_SHOT, "0");
-                write_sysfs(TX_TRIGGER, "timer");
-                write_sysfs(PWR_DELAY_ON, "100");
-                write_sysfs(PWR_DELAY_OFF, "9000");
-                state = "waiting";
-            }
-            this_thread::sleep_for(chrono::seconds(2));
-        }
+  while (true) {
+    if (fs::is_directory("/sys/class/net/" + eth_if)) {
+      state = "active";
+      running = true;
+      thread tx_thread(monitor_packets, eth_if, "tx", TX_LED_SHOT, "Data transmitted", "tx_monitor");
+      thread rx_thread(monitor_packets, eth_if, "rx", RX_LED_SHOT, "Data received", "rx_monitor");
+      tx_thread.join();
+      rx_thread.join();
+      running = false;
+    } else {
+      if (state != "waiting") {
+        cerr << "Network interface " << eth_if << " does not exist. Please check the interface name." << endl;
+        write_sysfs(RX_LED_SHOT, "1");
+        write_sysfs(TX_LED_SHOT, "0");
+        write_sysfs(TX_TRIGGER, "timer");
+        write_sysfs(PWR_DELAY_ON, "100");
+        write_sysfs(PWR_DELAY_OFF, "9000");
+        state = "waiting";
+      }
+      this_thread::sleep_for(chrono::seconds(2));
     }
-    return 0;
+  }
+  return 0;
 }
